ef419f7d020ca9549cae5552086856ac5f904e75
MatePiranishvili <matepiranishvili6@gmail.com>
2024-02-13 21:12:40 +0400

    commit

---
+++ homework2.py
@@ -0,0 +1,1 @@
+print("hello my name is mate i am memebr of goal-oriented-academy goal is best way to learn programing in fun and interesting way people in here are really friendly and i love being memeber of this club i met so many other people i am never leaving this club and  am goin to become pro programer ")
