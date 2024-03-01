052bc349dbfaefbd06c8ddd2ce022356fb373a0d
MatePiranishvili <matepiranishvili6@gmail.com>
2024-03-01 22:52:27 +0400

    commit

---
+++ კლასგარეშე სამუშაო/registration.py
@@ -0,0 +1,7 @@
+name=input("enter your name: ")
+surname=input("enter your surname: ")
+age=int(input("enter your age: "))
+email=input("enter your email: ")
+password=input("enter your password: ")
+
+print("hello,your name is",name , surname , "and you are" , age "years old" , "and your email is" , password , email)
