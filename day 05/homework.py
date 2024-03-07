9980c9b62d956a6492818a8fc19a116dbaf0ac2c
MatePiranishvili <matepiranishvili6@gmail.com>
2024-03-07 20:15:44 +0400

    commit

---
+++ day 05/homework.py
@@ -0,0 +1,21 @@
+num1=int(input("enter your number:" ))
+num2=int(input("enter your 2nd number: "))
+print(num1 / num2)
+ 
+
+num3=18
+num4=3
+print(num3 / num4 )
+
+#int=4
+#float=2.3
+#string="laptop"
+
+name="nintendo"
+print(type(name))
+number=12
+print(type(number))
+
+natural=2.6
+print(type(natural))
+
