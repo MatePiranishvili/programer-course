8b10fc224a4fee293ef89147cd777e3175b0771a
MatePiranishvili <matepiranishvili6@gmail.com>
2024-03-14 18:47:26 +0400

    commit

--- day 06/day 06/homework.py
+++ day 06/day 06/homework.py
@@ -1,12 +1,71 @@
-print(12>4 or 45<67)   
-print (900>899 or 1000<2000)
-print (365>243 or 134<349)
-print (4050>302 or 3435<4000)
-print (4560>200 or 3222<90203) 
-#ამ ორ შემთხვევეაში ერთში ვიყენებთ and ხოლო მეორე შემთხვევაში or ფუნქციებს
+print(345>213 and 567>234)    #მექნიკური შედარებები
+print(5134<6000 and 4579>2905)
+print(200>100 and 456>293)
+print(6890<7890 and 3456>245)
+print(3556<4565 and 6543>7890)
 
-print (2334>400 and 345<4000) 
-print (456>4000 and 342<4829)
-print (45566>100000 and 3435<20406)
-print (40000>500000 and 67050<500000 )
-print (90>80 and 60<90)
+
+print(456>564 or 787<890)
+print(333>223 or 765<764)
+print(132>76 or 980<349)
+print(619>620 or 678<231)
+print(5675<214 or 6789>3456)
+
+
+a=True and True #True
+b=True or True  #False
+
+
+num1=input("whats your number?:")
+num2=input("whats your 2nd number:")
+ 
+
+print(num1<num2)
+
+num1=(68)
+num2=(45)
+num3=(num1>num2)
+print(num1>num2)
+
+
+
+
+num1=34
+name="BMW"   
+num2=23.9
+num3=(123>312)       
+
+
+print(type(34))
+print(type("BMW"))
+print(type(23.9))
+print(type(123>312))
+
+name="Mate"
+number=19
+
+print (input("hello whats your name?:"))
+print (input("what is your GOA gruop numer?:"))
+
+num1=4/2
+print(type(num1))
+
+
+num1=20
+num2=24
+
+print(input("20"+"24"))
+
+num1=1.789
+num2=5.49
+num3=0.384
+num4=637.903
+num5=62.29
+
+
+print(num1,int(num1))
+print(num2, int(num2))
+print(num3, int(num3))
+print(num4, int(num4))
+print(num5, int(num5))
+
